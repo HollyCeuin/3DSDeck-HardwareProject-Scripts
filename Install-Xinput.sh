5f300dc309cebf80148f6869d3c4c01ca5c7337c
HollyCeuin <96226750+HollyCeuin@users.noreply.github.com>
2024-03-06 15:09:32 +0000

    Add files via upload

---
+++ Install-Xinput.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+echo disabling readonly
+sudo steamos-readonly disable
+echo readonly disabled
+sudo pacman-key --init
+sudo pacman-key --populate archlinux
+sudo pacman-key --populate holo
+sudo pacman -Sy archlinux-keyring holo-keyring
+sudo pacman -Sy xorg-xinput
+sudo pacman -Sy redshift
+echo fixing xinput
+xinput --map-to-output "wch.cn USB2IIC_CTP_CONTROL" "DisplayPort-0"
+echo enabling readonly
+sudo steamos-readonly enable
+echo done
+sleep 3
+exit 0
---
+++ QuickFix-Screen.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+xinput --map-to-output "wch.cn USB2IIC_CTP_CONTROL"  DisplayPort-0
+exit 0
