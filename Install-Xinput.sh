2a456c38794a75daacca37f0b684fe252355b5b5
HollyCeuin <96226750+HollyCeuin@users.noreply.github.com>
2024-03-07 12:39:02 +0000

    Update Install-Xinput.sh

--- Install-Xinput.sh
+++ Install-Xinput.sh
@@ -8,8 +8,6 @@ sudo pacman-key --populate holo
 sudo pacman -Sy archlinux-keyring holo-keyring
 sudo pacman -Sy xorg-xinput
 sudo pacman -Sy redshift
-echo fixing xinput
-xinput --map-to-output "wch.cn USB2IIC_CTP_CONTROL" "DisplayPort-0"
 echo enabling readonly
 sudo steamos-readonly enable
 echo done
